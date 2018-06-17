c84916d33a06f985b5a34c0884ef8bd5e4c1d5d1
bfv <dev@bfv.io>
2018-06-17 16:48:34 +0200

    about text when deployed via npm

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -15,7 +15,13 @@ export class AboutCommand implements Executable {
         console.log('\nxrefcli tool, OpenEdge XREF information from the command line');
         console.log('---------------------------------------------------------------');
         const pkginfo = require('pkginfo')(module, 'version', 'author');
+
+        if (module.exports['author']['name'] === undefined) {
         console.log('author: ', module.exports['author']);
+        }
+        else {
+            console.log('author: ', module.exports['author']['name'] + ' <' + module.exports['author']['emai'] + '>');
+        }
         console.log('version:', module.exports['version']);
     }
 
