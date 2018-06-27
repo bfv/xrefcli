2b66afffce61b0c4514a41a0ebcb876834e70fe3
bfv <dev@bfv.io>
2018-06-27 08:15:47 +0200

    #8 moved option handling to validate()

--- src/commands/repos.ts
+++ src/commands/repos.ts
@@ -4,28 +4,19 @@ import { Help } from '../help';
 
 export class ReposCommand implements Executable {
 
-    config: Config;
-
+    private config: Config;
+    private verbose = false;
+    private json = false;
     constructor(config: Config) {
         this.config = config;
     }
 
     execute(params: any): void {
 
-        const options = params['options'];
-        if (<boolean>options['help'] === true) {
-            const help = new Help();
-            help.reposCommand();
-            process.exit(0);
-        }
-
-        const name = <string> options['name'];
-        const verbose = <boolean> options['verbose'];
-
         const numIndent = 2;
 
-        if (<boolean>params['options']['json']) {
-            if (!verbose) {
+        if (this.json) {
+            if (!this.verbose) {
                 console.log(JSON.stringify(this.config.data.repos.map(repo => repo.name), undefined, numIndent));
             }
             else {
@@ -34,7 +25,7 @@ export class ReposCommand implements Executable {
         }
         else {
             this.config.data.repos.forEach(repo => {
-                if (!verbose) {
+                if (!this.verbose) {
                     console.log((repo.name === this.config.data.current ? '*' : '') + repo.name);
                 }
                 else {
@@ -45,6 +36,23 @@ export class ReposCommand implements Executable {
     }
 
     validate(params: any) {
+
+        const options = params['options'];
+
+        if (<boolean>options['help'] === true) {
+            const help = new Help();
+            help.reposCommand();
+            process.exit(0);
+        }
+
+        if (<boolean>options['verbose'] === true) {
+            this.verbose = true;
+        }
+
+        if (<boolean>options['json'] === true) {
+            this.json = true;
+        }
+
         return true;
     }
 
