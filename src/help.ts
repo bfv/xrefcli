c9574ac28b5bb559c6c8b967732ab44a78b3831d
bfv <dev@bfv.io>
2018-10-19 12:31:55 +0200

    #23 updated help

--- src/help.ts
+++ src/help.ts
@@ -24,6 +24,7 @@ export class Help {
                     { name: 'export', summary: 'Exports source/table usage to CSV' },
                     { name: 'init', summary: 'Initializes repo' },
                     { name: 'list', summary: 'List tables/database used in current repo' },
+                    { name: 'matrix', summary: 'Creates CSV of table usage by sources'},
                     { name: 'parse', summary: 'Parses .xref files of repo' },
                     { name: 'remove', summary: 'Removes a repo' },
                     { name: 'repos', summary: 'List the available repos' },
